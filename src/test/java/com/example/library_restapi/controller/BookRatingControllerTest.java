2cb503e671ebcbe61190df9cf91809d7e58eb9e2
SamiraKetabifard <samiraketabi.fard@gmail.com>
2026-01-29 21:04:57 +0330

    done

--- src/test/java/com/example/library_restapi/controller/BookRatingControllerTest.java
+++ src/test/java/com/example/library_restapi/controller/BookRatingControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookRatingController.class)
-@AutoConfigureMockMvc(addFilters = false)
+//@AutoConfigureMockMvc(addFilters = false)
 class BookRatingControllerTest {
 
     @Autowired
